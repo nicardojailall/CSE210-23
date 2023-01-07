36347bb210442fdd7ea457d8dbc8894594faa670
Jailall Nicardo <Nicardo.Jailall@saipem.com>
2023-01-07 08:59:34 -0400

    Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+      Console.Write("What is your Grade Percentage: ");
+        string grade = Console.ReadLine();  
+        int percent =  int.Parse(grade);
+        string mark = "";
+
+        if (percent >= 90)
+        {
+            mark = "A";
+        }
+        else if (percent >= 80)
+        {
+            mark = "B";
+        }
+        else if (percent >= 70)
+        {
+            mark = "C";
+        }
+        else if (percent >= 60)
+        {
+            mark = "D";
+        }
+        else
+        {
+            mark = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {mark}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
