5ca43ea8e48ec1843d6f4bb6bb39392bcb8efe1c
nicardojailall <nicardo.nj@gmail.com>
2023-01-09 15:38:27 -0400

    Finished

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int randonNumGen = randomGenerator.Next(1, 101);
+        int magicNum= -1;
+
+        while (magicNum != randonNumGen)
+        {
+            Console.Write("What is your guess? ");
+            magicNum = int.Parse(Console.ReadLine());
+
+            if (randonNumGen > magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (randonNumGen < magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }  
     }
 }
